10d8e0aa1f999173813bff06750690c0d17cb64a
Seva Zhidkov <zhidkovseva@gmail.com>
2016-02-26 01:48:18 +0400

    Add first version

---
+++ ross/templates/recipes.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: Seva Zhidkov
+@contact: zhidkovseva@gmail.com
+@license: Apache License 2.0
+Copyright (C) 2015
+"""
+
+en_templates = (
+                # How to cook a cake?
+                (r'how ?(to|i can)? cook (.+)',
+                 lambda m: {'type': 'recipes',
+                            'subtype': 'search',
+                            'action': 'question',
+                            'query': m.group(2)}),
+                # Cook me cake
+                (r'cook ?(me)? (.+)',
+                 lambda m: {'type': 'recipes',
+                            'subtype': 'search',
+                            'action': 'request',
+                            'query': m.group(2)}),
+                # Find me recipe of cake
+                (r'((find|search) ?(me)? )?(recipes? of) (.+)',
+                 lambda m: {'type': 'recipes',
+                            'subtype': 'search',
+                            'action': 'question',
+                            'query': m.group(5)}),
+                # I want cake recipes
+                (r'(find|search|i (need|want)) ?(me)? (.+) recipes?',
+                 lambda m: {'type': 'recipes',
+                            'subtype': 'search',
+                            'action': 'question',
+                            'query': m.group(4)}),
+                # I am planning to cook a cake
+                (r'(i |i (am|\'m)? )?(want|need|must|should) ?(to)? cook (.+)',
+                 lambda m: {'type': 'recipes',
+                            'subtype': 'search',
+                            'action': 'info',
+                            'query': m.group(5)}),
+                # I will cook a cake
+                (r'i((\'m| am) plann?(ing)? to| will) cook (.+)',
+                 lambda m: {'type': 'recipes',
+                            'subtype': 'search',
+                            'action': 'plans',
+                            'query': m.group(4)}),
+               )
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'ross',
+  packages = ['ross', 'ross.templates'],
+  version = '0.1.0',
+  description = 'Small library for processing people requests for bot based on regular expressions',
+  author = 'Seva Zhidkov',
+  author_email = 'zhidkovseva@gmail.com',
+  url = 'https://github.com/leonardbot/ross',
+  download_url = 'https://github.com/leonardbot/ross/tarball/0.1.0',
+  keywords = ['nlp', 'language', 'bot'],
+  classifiers = [],
+)
