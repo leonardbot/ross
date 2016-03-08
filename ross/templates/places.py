c7b76b664f1eb2f92c925e1139239461f5ffdd34
Seva Zhidkov <zhidkovseva@gmail.com>
2016-03-08 16:52:56 +0400

    add russian places templates

--- ross/templates/places.py
+++ ross/templates/places.py
@@ -20,4 +20,25 @@ en_templates = (
                            'subtype': 'search',
                            'action': 'question',
                            'query': m.group(3)}),
+                # Where i can go today?
+                (r'where (i can|can i|to) go',
+                 lambda m: {'type': 'places',
+                            'subtype': 'explore',
+                            'action': 'question',
+                            'query': None}),
+               )
+
+ru_templates = (
+                # Где здесь находится ближайший Старбакс?
+                (r'где( здесь)? (находится|есть)( ближайший)? (.+)',
+                 lambda m: {'type': 'places',
+                            'subtype': 'search',
+                            'action': 'question',
+                            'query': m.group(4)}),
+                # Куда мне сходить сегодня вечером?
+                (r'куда( мне)? сходить',
+                 lambda m: {'type': 'places',
+                            'subtype': 'explore',
+                            'action': 'question',
+                            'query': None}),
                )
